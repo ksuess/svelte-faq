02b2d4e085a7c67158725d3bd1cb831fb11c6c3d
Katja Suess <k.suess@rohberg.ch>
2020-01-30 17:55:34 +0100

    toggle faq answer (transition fade)

--- src/FAQItem.svelte
+++ src/FAQItem.svelte
@@ -3,14 +3,22 @@
 
   export let question = "What is the question?"
   export let answer = "The answer is 42."
+
+  let visible = false;
+
+  function toggleAnswer(event) {
+    visible = !visible;
+  }
 </script>
 
 
-<li transition:fade="{{duration:2000}}">
-  <h2>{question}</h2>
-  <p>
-    {answer}
-  </p>
+<li transition:fade="{{duration:1000}}">
+  <h2 on:click={toggleAnswer}>{question}</h2>
+  {#if visible}
+    <p transition:fade="{{duration:1000}}">
+      {answer}
+    </p>
+  {/if}
 </li>
 
 
