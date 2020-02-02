b6ff9ddfe079fec4f50ab43d1abc947b87d7ab0e
Katja Suess <k.suess@rohberg.ch>
2020-02-02 11:20:00 +0100

    move store creation from stores.js (definition, no more) to App.svelte where components are used / instanciated
    next step: using external data.

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,42 @@
 <script>
   import FAQ from "./FAQ.svelte"
-  import {faqitemsstore1} from './stores.js'
-  import {faqitemsstore2} from './stores.js'
+  // import {faqitemsstore1} from './stores.js'
+  // import {faqitemsstore2} from './stores.js'
+  import {createFAQItems} from './stores.js'
 
   export let name;
+
+
+  // stores for multiple FAQs
+  const faqitems_plone = [
+      {
+          question: 'What does the Plone Foundation do?',
+          answer: `The mission of the Plone Foundation is to protect and promote Plone.
+              The Foundation provides marketing assistance, awareness, and
+              evangelism assistance to the Plone community. The Foundation also
+              assists with development funding and coordination of funding for
+              large feature implementations. In this way, our role is similar to
+              the role of the Apache Software Foundation and its relationship with
+              the Apache Project.`
+      },
+      {
+          question: 'Who can join the Plone Foundation?',
+          answer: `Everyone contributing to Plone Software, Plone documentation, organizing events or doing something good for PF.`
+      },
+      {
+          question: 'When is the next conference?',
+          answer: `November in Belgium`
+      }
+  ]
+
+  const faqitems_frameworks = [
+      {
+          question: 'Why do I need a framework?',
+          answer: 'It saves time. You can skip to important tasks.'
+      }
+  ]
+  const faqstore1 = createFAQItems(faqitems_plone);
+  const faqstore2 = createFAQItems(faqitems_frameworks);
 </script>
 
 
@@ -13,25 +46,30 @@
     <ul>
       <li>
         <button on:click={() => {
-          console.log(faqitemsstore1);
-          console.log($faqitemsstore1)
-        }}>FAQ "{name}": log store faqitemsstore1</button>
+          console.log(faqstore1);
+          console.log($faqstore1)
+        }}>FAQ "{name}": log store of FAQs 1</button>
       </li>
       <li>
         <button on:click={() => {
-          console.log(faqitemsstore2);
-          console.log($faqitemsstore2)
-        }}>FAQ "Frameworks" log store faqitemsstore2</button>
+          console.log(faqstore2);
+          console.log($faqstore2)
+        }}>FAQ "Frameworks" log store of FAQs 2</button>
       </li>
+      <!-- <li>
+        <button on:click={() => {
+            console.log("piep")
+          }}>show</button>
+      </li> -->
     </ul>
 
   </div>
 
   <h1>FAQ for "{name}"</h1>
-  <FAQ faqitems={faqitemsstore1}/>
+  <FAQ faqitems={faqstore1}/>
 
   <h1>FAQ for "Frameworks"</h1>
-  <FAQ faqitems={faqitemsstore2}/>
+  <FAQ faqitems={faqstore2}/>
 </main>
 
 
@@ -51,7 +89,7 @@
   }
 
   .debug {
-    display: none;
+    /* display: none; */
     background-color: lighten(seagreen, 45%);
     padding: 1rem;
     li {
--- src/stores.js
+++ src/stores.js
@@ -1,38 +1,8 @@
 import {writable} from 'svelte/store'
 
-const faqitems_default = [
-    {
-        question: 'What does the Plone Foundation do?',
-        answer: `The mission of the Plone Foundation is to protect and promote Plone.
-            The Foundation provides marketing assistance, awareness, and
-            evangelism assistance to the Plone community. The Foundation also
-            assists with development funding and coordination of funding for
-            large feature implementations. In this way, our role is similar to
-            the role of the Apache Software Foundation and its relationship with
-            the Apache Project.`
-    },
-    {
-        question: 'Who can join the Plone Foundation?',
-        answer: `Everyone contributing to Plone Software, Plone documentation, organizing events or doing something good for PF.`
-    },
-    {
-        question: 'When is the next conference?',
-        answer: `November in Belgium`
-    }
-]
-
-const faqitems_frameworks = [
-    {
-        question: 'Why do I need a framework?',
-        answer: 'It saves time. You can skip to important tasks.'
-    }
-]
-
-
-
 // store with args for initial data
 // with args it's possible to use function for multiple independent stores of same type for same component which is consumed more than once
-function createFAQItems(items) {
+export function createFAQItems(items) {
     const {subscribe, set, update} = writable(items);
 
     return {
@@ -54,6 +24,3 @@ function createFAQItems(items) {
         reset: () => set(items)
     }
 }
-
-export const faqitemsstore1 = createFAQItems(faqitems_default);
-export const faqitemsstore2 = createFAQItems(faqitems_frameworks);
